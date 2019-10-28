277e710272786e5096dfe9353b2663ee76fd246e
Muhammad Feisal Shibiby <37222375+Shibiby@users.noreply.github.com>
2019-10-28 16:13:01 +0300

    first commit

---
+++ config/database.js
@@ -0,0 +1,13 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost', //host 
+        'user': 'root', // database username
+        'password': '123456', // database password
+        'database': 'node_app'//database name
+    },
+	'database': 'node_app',
+    
+};
+
+	
